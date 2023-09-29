175deeb46f802975e00aece12c3c844a4aea8f4f
Mohammad Nadeem <mohammednadeem902@gmail.com>
2023-09-29 22:41:54 +0000

    initialized basic next js project with mantine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,74 +1,34 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import { Button, Box, Center, Container, Header, Group, rem } from "@mantine/core";
+
+const HEADER_HEIGHT = rem(60);
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
+      <Header height={HEADER_HEIGHT} sx={{ borderBottom: 0 }} mb={120}>
+        <Container fluid>
+          <Group>
+            masjids.io
+          </Group>
+          <Group>
+            <Button radius="xl" h={20}>
+              Sign Up/Login
+            </Button>
+          </Group>
+        </Container>
+      </Header>
       <Head>
         <title>masjids.io</title>
+        <meta name="description" content="masjids.io home page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to masjids.io!
-        </h1>
-      </main>
-
-      <footer>
-        Made with ❤️ by the masjids.io team.
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      <Container className="box">
+        <Center h={724} mx="auto">
+          <Box>TO DO</Box>
+        </Center>
+      </Container>
+    </>
+  );
 }
