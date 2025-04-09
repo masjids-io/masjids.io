3d3bdb2b05624d0d8d280075a0c368002421a050
mnadev <mohammednadeem902@gmail.com>
2025-04-09 18:51:25 -0400

    updated apps page

--- pages/apps.tsx
+++ pages/apps.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Container } from '@mantine/core';
+import { Container, SimpleGrid } from '@mantine/core';
 import { ApplicationCard } from '../components/application_card';
 import { Navbar } from '../components/navbar';
 
@@ -13,19 +13,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container className="box">
-        <ApplicationCard
-          title="nikkah.io"
-          description="A modern Islamic matrimonial platform"
-          image="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
-          url="nikkah.io"
-        ></ApplicationCard>
-        <ApplicationCard
-          title="adhan.io"
-          description="An innovative adhan app"
-          image="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
-          url="adhan.io"
-        ></ApplicationCard>
+      <Container>
+        <SimpleGrid cols={3} spacing="xl" breakpoints={[
+          { maxWidth: 'sm', cols: 1, spacing: 'md' },
+          { maxWidth: 'md', cols: 2, spacing: 'md' },
+          { maxWidth: 'lg', cols: 3, spacing: 'xl' },
+        ]}>
+          <ApplicationCard
+            title="masjids.io"
+            description="A modern cloud platform for masjids."
+            image="/images/masjidnikkah.jpeg"
+            url="masjids.io"
+          />
+          <ApplicationCard
+            title="nikkah.io"
+            description="A matrimonial platform for Muslims designed ground up to match the needs of the Muslim community."
+            image="/images/masjidnikkah.jpeg"
+            url="nikkah.io"
+          />
+          <ApplicationCard
+            title="adhan.io"
+            description="An innovative adhan app to foster greater masjid engagement."
+            image="/images/muezzin.jpeg"
+            url="adhan.io"
+          />
+        </SimpleGrid>
       </Container>
     </>
   );
