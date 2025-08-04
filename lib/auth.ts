341924b8dbad84c7e427c4539877e60bbbf9ec13
yusuf <yusufyahya0107@gmail.com>
2025-08-04 11:59:26 +0800

    add 60 minutes at user session

--- lib/auth.ts
+++ lib/auth.ts
@@ -90,7 +90,7 @@ export const authOptions: NextAuthOptions = {
   ],
   session: {
     strategy: "jwt",
-    maxAge: 15 * 1,
+    maxAge: 60 * 60,
   },
   callbacks: {
     async jwt({ token, user }) {
